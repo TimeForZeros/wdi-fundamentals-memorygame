df1e7eb65a77dfc2309ff866de10a47e18bc839f
Victor <dazedbutamused@gmail.com>
2019-07-16 11:41:23 -0700

    Included functions to streamline JS

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,25 +7,28 @@ var cardFour = "king";*/
 //console.log("user flipped " + cardTwo);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2)
+function checkForMatch()
 {
-    console.log("This is working!");
+    if (cardsInPlay.length === 2)
+    {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Sorry, try again.");
+        }
+    }
+    else
+    {
+        return;
+    }
 }
-else 
+function flipcard(cardID)
 {
-    console.log("This isn't working...");
+  
+   console.log( "User flipped " + cards[cardID]);
+   cardsInPlay.push(cards[cardID]);
+   checkForMatch();
+
 }
-if (cardsInPlay[0] === cardsInPlay[1])
-{
-    alert("You found a match!");
-}
-else 
-{
-    alert("Sorry, try again.");
-}
+flipcard(0);
+flipcard(2);
